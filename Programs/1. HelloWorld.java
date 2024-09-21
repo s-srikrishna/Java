e04154c4ed673a4e6b05d7026beb92b2207d4a26
S Sri Krishna <s.srikrishna.official@gmail.com>
2024-09-21 19:05:53 +0530

    Program_001 - Print "Hello World" on the console

---
+++ Programs/1. HelloWorld.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+// program_001 - Print "Hello World" on the console
+class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
