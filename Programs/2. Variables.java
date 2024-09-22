c404701cd8bd3c15858c37dc111129519f3ccd49
S Sri Krishna <s.srikrishna.official@gmail.com>
2024-09-23 04:46:44 +0530

    2. Variables.java

---
+++ Programs/2. Variables.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+// Program 002 - Declare and Print Variables
+class Variables {
+    public static void main(String[] args){
+        int age = 22;
+        float weight = 75.4f;
+        String name = "Sri Krishna";
+        System.out.println(age + "\n" + weight + "\n" + name);
+    }
+}
